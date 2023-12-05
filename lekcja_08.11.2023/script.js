2a087b4bff2e7874b13e77173a5af8ae8dc16400
KacperOtreba <kacperotreba1@gmail.com>
2023-12-05 23:53:24 +0100

    oporne ćwiczenia

--- lekcja_08.11.2023/script.js
+++ lekcja_08.11.2023/script.js
@@ -4,42 +4,43 @@ function dodawanie(){
     let suma = liczba1 -+- liczba2;
     document.getElementById("suma").innerText = suma;
 }
-// function formularz(){
-//     let a = document.getElementById("1").value;
-//     let b = document.getElementById("2").value;
-//     let pole = a * b;
-//     let wynik = document.getElementById("wynik");
-//     wynik.innerText = "Wynik to: " + pole;
-// }
 
-// function formularz2(){
-//     let f = document.getElementsByName("radio");
-//     let v;
-//     for (i = 0; i < f.length; i++){
-//         if (f[i].checked){
-//             v = f[i].value;
-//         }
-//     }
-//     alert(v);
-// }
+function formularz(){
+    let a = document.getElementById("1").value;
+    let b = document.getElementById("2").value;
+    let pole = a * b;
+    let wynik = document.getElementById("wynik");
+    wynik.innerText = "Wynik to: " + pole;
+}
 
-// function formularz3(){
-//     let f = document.getElementsByName("ptaki");
-//     let v = '';
-//     for (i = 0; i < f.length; i++){
-//         if (f[i].checked){
-//             v = v+" "+ f[i].value;
-//         }
-//     }
-//     if (v==''){
-//         v="Nie zaznaczyłeś";
-//     }
-//     alert(v);
-// }
+function formularz2(){
+    let f = document.getElementsByName("radio");
+    let v;
+    for (i = 0; i < f.length; i++){
+        if (f[i].checked){
+            v = f[i].value;
+        }
+    }
+    alert(v);
+}
 
-// function formularz4(){
-//     let s = document.getElementById("zwierz");
-//     alert("Wartośc wybranej opcji to: " + s.value);
-//     alert("Indeks wybranej rzeczy to: " + s.selectedIndex);
-//     alert("Długość selecta to: " + s.length);
-// }
+function formularz3(){
+    let f = document.getElementsByName("ptaki");
+    let v = '';
+    for (i = 0; i < f.length; i++){
+        if (f[i].checked){
+            v = v+" "+ f[i].value;
+        }
+    }
+    if (v==''){
+        v="Nie zaznaczyłeś";
+    }
+    alert(v);
+}
+
+function formularz4(){
+    let s = document.getElementById("zwierz");
+    alert("Wartośc wybranej opcji to: " + s.value);
+    alert("Indeks wybranej rzeczy to: " + s.selectedIndex);
+    alert("Długość selecta to: " + s.length);
+}
---
+++ przedSpr2/script.js
@@ -0,0 +1,62 @@
+function formularz(){
+    let wybor = document.getElementById("selekt");
+    if (wybor.selectedIndex == 0){
+        PierwszaCzyZlozona();
+    }
+    else if (wybor.selectedIndex == 1){
+        LiterkaZTekstu();
+    }
+    else if (wybor.selectedIndex == 2){
+        LosoweLiczby();
+    }
+    else if (wybor.selectedIndex == 3){
+        GeneratorTabelki();
+    }
+}
+
+function PierwszaCzyZlozona(){
+    let number = document.getElementById("number").value;
+    let p = 0;
+    for (let i = 2; i < number; i++){
+        if (number % i == 0){
+            p++;
+        }
+    }
+    if (p > 0){
+        alert("Liczba jest złożona")
+    }
+    if (p == 0){
+        alert("Liczba jest pierwsza")
+    }
+}
+
+function LiterkaZTekstu(){
+    let text = document.getElementById("text").value
+    let number = document.getElementById("number").value
+    let info = document.getElementById("info")
+    info.textContent = text[number]
+}
+
+function LosoweLiczby(){
+    let number = document.getElementById("number").value
+    let info = document.getElementById("info")
+    info.innerText = "";
+    for (let index = 0; index < number; index++){
+        info.append(parseInt(Math.random()*100) + " ")
+    }
+}
+
+function GeneratorTabelki(){
+    let number = document.getElementById("number").value
+    document.write("<table>")
+    for (let index = 0; index < number; index++) {
+        document.write("<tr><td>"+"0"+"</td></tr>")
+    }
+    document.write("</table>")
+}
+
+function zmianakoloru(){
+    let kolor = document.getElementById("kolor").value
+    let body = document.getElementById("1")
+    body.style.backgroundColor = kolor
+}
